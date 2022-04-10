c4f80d95080acf08e2644723a560c4ba589e831b
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-04-10 16:15:55 +0530

    Update marks-of-pcm.cpp

--- searching&sorting/marks-of-pcm.cpp
+++ searching&sorting/marks-of-pcm.cpp
@@ -3,6 +3,62 @@
 //
 
 
+class Marks{
+    public:
+    int phy;
+    int chem;
+    int math;
+    
+    Marks() {
+        phy = 0;
+        chem = 0;
+        math = 0;
+    }
+    Marks(int x, int y, int z) {
+        phy = x;
+        chem = y;
+        math = z;
+    }
+};
+
+class Compare {
+    public:
+    bool operator() (Marks& a, Marks& b) { 
+        if(a.phy != b.phy) {
+            return a.phy < b.phy;
+        }
+        else if(a.chem != b.chem) {
+            return a.chem > b.chem;
+        }
+        else {
+            return a.math < b.math;
+        }
+    }
+};
+
+class Solution{
+    public:
+    void customSort (int phy[], int chem[], int math[], int N)
+    {
+        vector<Marks> a(N);
+        for(int i = 0; i < N; i++) {
+            Marks m(phy[i], chem[i], math[i]);
+            a[i] = m;
+        }
+        
+        sort(a.begin(), a.end(), Compare());
+        
+        for(int i = 0; i < N; i++) {
+            phy[i] = a[i].phy;
+            chem[i] = a[i].chem;
+            math[i] = a[i].math;
+        }
+    } 
+};
+
+
+// ==== using lambda function ====
+
 class Solution{
     public:
     void customSort (int phy[], int chem[], int math[], int N)
