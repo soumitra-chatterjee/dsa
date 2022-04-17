3413edd301821bf855bd0aabd8608d27fcd0512c
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-04-17 16:10:19 +0530

    Create trim-bst.cpp

---
+++ Leetcode/Tree/669. Trim the BST/trim-bst.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == nullptr) {
+            return root;
+        }
+        if(root->val < low) {
+            return trimBST(root->right, low, high);
+        }
+        else if(root->val > high) {
+            return trimBST(root->left, low, high);
+        }
+        else {
+            root -> left = trimBST(root->left, low, high);
+            root -> right = trimBST(root->right, low, high);
+        }
+        return root;
+    }
+};
