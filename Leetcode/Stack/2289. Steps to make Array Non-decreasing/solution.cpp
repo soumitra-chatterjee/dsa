add3d72f7aca388f659334ee658ed1757ebf2992
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-06-10 00:58:47 +0530

    steps to make the array decreasing

---
+++ Leetcode/Stack/2289. Steps to make Array Non-decreasing/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+    const int INF = 1e9 + 1;
+public:
+    int totalSteps(vector<int>& nums) 
+	{
+        int N = nums.size();
+		
+        stack<pair<int, int>> stk;  // -> {val, count}
+        stk.emplace(INF, 0);
+        
+		int result = 0;
+        for(int i = N-1; i >= 0; i--) {
+            int delCount = 0;
+            while(nums[i] > stk.top().first) 
+			{
+                int prevCount = stk.top().second;
+                delCount = max(delCount+1, prevCount);
+                stk.pop();
+            }
+            res = max(res, delCount);
+            stk.emplace(nums[i], delCount);
+        }
+        return res;
+        
+    }
+};
