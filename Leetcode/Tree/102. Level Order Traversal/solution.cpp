4272937254854811d917e6be67c74864ef851d0f
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-06-16 16:10:40 +0530

    level order traversal in binary tree

---
+++ Leetcode/Tree/102. Level Order Traversal/solution.cpp
@@ -0,0 +1,62 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder1(TreeNode* root) {
+        if(root == nullptr) {
+            return {};
+        }
+        vector<int> tmp;
+        vector<vector<int>> res;
+        queue<TreeNode*> q;
+        q.push(root);
+        while(!q.empty()) {
+            int n = q.size();
+            while(n--) {        
+                TreeNode* curr = q.front();
+                q.pop();
+                tmp.push_back(curr->val);
+                if(curr->left != nullptr) 
+                    q.push(curr->left);
+                if(curr->right != nullptr) 
+                    q.push(curr->right);
+            }
+            res.push_back(tmp);
+            tmp.clear();
+        }
+        return res;
+    }
+    
+    // better time 
+    vector<vector<int>> levelOrder2(TreeNode* root) {
+        if(root == nullptr) {
+            return {};
+        }
+        vector<vector<int>> res;
+        vector<int> tmp;
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(nullptr);
+        while(!q.empty()) {
+            TreeNode* curr = q.front();
+            q.pop();
+           
+            if(curr == nullptr) {
+                res.push_back(tmp);
+                tmp.clear();
+                if(q.size() == 0)
+                    return res;
+                q.push(curr);
+                continue;
+            }
+            tmp.push_back(curr->val);
+            
+            if(curr -> left != nullptr) 
+                q.push(curr->left);
+            
+            if(curr->right != nullptr) 
+                q.push(curr->right);
+            
+            
+        }
+        return res;
+    }
+};
