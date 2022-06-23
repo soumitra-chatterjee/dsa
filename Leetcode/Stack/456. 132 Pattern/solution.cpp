c94a4e3ecc4ff57cc596237a17526bc273c53758
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-06-23 20:08:42 +0530

    Update solution.cpp

--- Leetcode/Stack/456. 132 Pattern/solution.cpp
+++ Leetcode/Stack/456. 132 Pattern/solution.cpp
@@ -1,7 +1,6 @@
 class Solution {
 public:
-    bool find132pattern(vector<int>& nums) 
-	{
+    bool find132pattern(vector<int>& nums) {
         int N = nums.size();
 		
 		// minimum before current index
