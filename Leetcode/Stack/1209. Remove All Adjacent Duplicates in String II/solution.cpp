dbc9aff20e0b8aa0cd9bd609cecded02b83b8edd
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-06-23 20:19:27 +0530

    indentation errors

--- Leetcode/Stack/1209. Remove All Adjacent Duplicates in String II/solution.cpp
+++ Leetcode/Stack/1209. Remove All Adjacent Duplicates in String II/solution.cpp
@@ -1,30 +1,29 @@
 
 class Solution {
 public:
-    string removeDuplicates(string s, int k) 
-	{
-		string res;
+    string removeDuplicates(string s, int k) {
+	string res;
 
-		// stack will contain index, count pair
-		// stack -> {last distinct character index, duplicate count}
-		stack<pair<int, int>> count;
+	// stack will contain index, count pair
+	// stack -> {last distinct character index, duplicate count}
+	stack<pair<int, int>> count;
 
-		for(int i = 0; i < s.size(); i++) {
-			if(i == 0 || count.empty() || s[i] != s[count.top().first]) {
+	for(int i = 0; i < s.size(); i++) {
+		if(i == 0 || count.empty() || s[i] != s[count.top().first]) {
 			count.emplace(i, 1);
 			res.push_back(s[i]);
-			}
-			else {
-				count.top().second += 1;
-				res.push_back(s[i]);
-				if(count.top().second == k) {
-					for(int i = 1; i <= k; i++) {
-						res.pop_back();
-					}
-					count.pop();
+		}
+		else {
+			count.top().second += 1;
+			res.push_back(s[i]);
+			if(count.top().second == k) {
+				for(int i = 1; i <= k; i++) {
+					res.pop_back();
 				}
+				count.pop();
 			}
 		}
-		return res;
-    }
+	}
+	return res;
+	}	
 };
