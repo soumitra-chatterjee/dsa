24f8d07d7b6bc4b95727d7540b32175c4042d942
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2022-07-15 21:15:20 +0530

    Update solution.cpp

--- Leetcode/Stack/1209. Remove All Adjacent Duplicates in String II/solution.cpp
+++ Leetcode/Stack/1209. Remove All Adjacent Duplicates in String II/solution.cpp
@@ -1,7 +1,7 @@
 
 class Solution {
 public:
-    string removeDuplicates(string s, int k) {
+    string removeDuplicates1(string s, int k) {
 	string res;
 
 	// stack will contain index, count pair
@@ -25,5 +25,26 @@ public:
 		}
 	}
 	return res;
-	}	
+	}
+	
+	string removeDuplicates2(string s, int k) {
+        string res = "";
+        vector<pair<char, int>> stk;  // {prev distinct character, count}
+        
+		for(char& c: s) {
+            if(!stk.empty() && c == stk.back().first) {
+                stk.back().second += 1;
+                if(stk.back().second == k) {
+                    stk.pop_back();
+                }
+            }
+            else {
+                stk.push_back({c, 1});
+            }
+        }
+        for(auto& pr : stk) {
+            res.append(pr.second, pr.first);
+        }
+        return res;
+    }
 };
