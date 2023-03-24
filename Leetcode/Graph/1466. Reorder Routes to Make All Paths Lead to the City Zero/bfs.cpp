fba779f3136dc8658748cfa5787848d3c8591668
Soumitra Chatterjee <soumitra.chatterjee.code1365@gmail.com>
2023-03-25 01:19:29 +0530

    bfs on tree

---
+++ Leetcode/Graph/1466. Reorder Routes to Make All Paths Lead to the City Zero/bfs.cpp
@@ -0,0 +1,30 @@
+class ReorderRoutes {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<int> G[n]; // adjacency list
+        set<int> d[n];  // lookup set for direcetions
+        for(vector<int>& v: connections) {
+            G[v[0]].push_back(v[1]);
+            G[v[1]].push_back(v[0]);
+            d[v[0]].insert(v[1]);
+        }
+        int cnt = 0;
+        queue<pair<int, int>> q;
+        q.push(make_pair(-1, 0));
+        while(q.empty() == false) {
+            int prev = q.front().first;
+            int cur = q.front().second;
+            q.pop();
+            for(int next: G[cur]) {
+                if(next == prev) {
+                    continue;
+                }
+                if(d[cur].find(next) != d[cur].end()) {
+                    cnt++;
+                }
+                q.push(make_pair(cur, next));
+            }
+        }
+        return cnt;
+    }
+};
